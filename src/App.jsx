62252a3243456231d0eb5f331329c5644e8b76dc
jiyea0904 <jiyea.park0904@gmail.com>
2024-12-06 15:42:20 +0900

    ✨ Mission1 - swiper
    도전미션 swiper 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,35 @@ import { Link, Route, Routes } from 'react-router-dom';
 import MovieDetail from './components/MovieDetail';
 import Layout from './components/Layout';
 import './App.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import "swiper/css"; // Core styles
+import "swiper/css/navigation"; // Navigation styles
+
+
 
 const MovieList = () => {
-  const [movies, setMovies] = useState(movieListData.results);
+  const [movies] = useState(movieListData.results);
   
   return(
     <div className='movielist'>
-      {movies.map((movie) => (
-        <Link
-          to="/details"
-          key={movie.id}
-          style={{ textDecoration: "none", color: "inherit" }}
-        >
-          <MovieCard title={movie.title} poster={movie.poster_path} rating={movie.vote_average} />
-        </Link>
-      ))}
+      <Swiper 
+        modules={[Navigation]}
+        spaceBetween={60}
+        slidesPerView="auto"
+        navigation>
+        {movies.map((movie) => (
+          <SwiperSlide key={movie.id} style={{width: '230px'}}>
+            <Link
+              to="/details"
+              key={movie.id}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <MovieCard title={movie.title} poster={movie.poster_path} rating={movie.vote_average} />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
